cf39539bfbeb5ff3194e20f407553db831beb033
Hebert Nunes <hebertcunha2@gmail.com>
2018-10-31 14:26:28 -0300

    Metodo para deletar produto

--- src/main/java/com/produtos/apirest/controllers/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controllers/ProdutoController.java
@@ -3,6 +3,7 @@ package com.produtos.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +34,10 @@ public class ProdutoController {
 	@PostMapping("/produto")
 	public Produto salvarProduto(@RequestBody Produto produto){
 		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto")
+	public void deletarProduto(@RequestBody Produto produto){
+		produtoRepository.delete(produto);
 	}
 }
